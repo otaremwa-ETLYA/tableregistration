be2db797c2c63c7363c66ef9bbf80811abfe277a
otaremwa-ETLYA <otaremwa@pureflowboda.com>
2026-04-06 20:04:38 +0300

    prefernces

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "40143019d7fba5af675865c5894cb592
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "54b8a608376501c8aaa9f188c83fb7c8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f80ebdefdc6e40401236dbde7c6a891d",
+"flutter_bootstrap.js": "ffb9d60689767ed9a4020b832d8d0f3e",
 "icons/Icon-192.png": "6fca841c69f44eeee61d5ec7d15637bf",
 "icons/Icon-512.png": "6fca841c69f44eeee61d5ec7d15637bf",
 "icons/Icon-maskable-192.png": "6fca841c69f44eeee61d5ec7d15637bf",
 "icons/Icon-maskable-512.png": "6fca841c69f44eeee61d5ec7d15637bf",
 "index.html": "976c9231b432f6528f473fbe4086b4a4",
 "/": "976c9231b432f6528f473fbe4086b4a4",
-"main.dart.js": "8473c99bb2980db0b55317aa4bd1436f",
+"main.dart.js": "b74c5339af442a97b3645570881a2847",
 "manifest.json": "f60a0dedd444f9a9bac9fec93b291fa0",
 "version.json": "f9c4448f0b4403ddeaa44a6862843969"};
 // The application shell files that are downloaded before a service worker can
