9b4612ef7f930dd94fb642da536c76ee97836923
otaremwa-ETLYA <otaremwa@pureflowboda.com>
2026-04-26 12:32:09 +0300

    all branches 1.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "896379531"
+    serviceWorkerVersion: "1795857054"
   }
 });
